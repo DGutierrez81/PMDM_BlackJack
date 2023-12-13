65d8499e6c5e1710854dd23836a3a9291c337483
DGutierrez81 <DGutierrezbueno81@gmail.com>
2023-12-13 14:06:56 +0100

    Casi terminado

--- app/src/main/java/com/example/blackjack/Palos.kt
+++
@@ -1,5 +0,0 @@
-package com.example.blackjack
-
-enum class Palos {
-    CORAZONES, DIAMANTES, TREBOLES, PICAS
-}
--- app/src/main/java/com/example/blackjack/model/Routes.kt
+++
@@ -1,6 +0,0 @@
-package com.example.blackjack.model
-
-
-sealed class Routes(val route: String) {
-    object Screen1: Routes("Screen1")
-}
